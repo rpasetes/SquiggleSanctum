0fdb284ab3fd07201f876d8c8683bb79e635eb21
rpasetes <russap01@gmail.co>
2022-11-22 11:20:26 +0800

    saving text to localstorage

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,8 +1,16 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css"
 import React from "react";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const Editor = () => {
+  const [text, setText] = useLocalStorage<string>("text", "");
+
+  const updateText = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const val = event.target.value;
+
+    setText(val);
+  };
 
   return (
     <div className={styles.container}>
@@ -14,7 +22,12 @@ const Editor = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>SquiggleSanctum</h1>
-        <textarea className={styles.gamearea}/>
+
+        <textarea 
+          className={styles.gamearea}
+          onChange={updateText}
+          value={text}
+        ></textarea>
       </main>
     </div>
   )
